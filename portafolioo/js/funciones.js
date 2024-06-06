ff3a8e5c667d7bdfd73d8821fde5873e7748126e
JIBEEEEE <111766279+JIBEEEEE@users.noreply.github.com>
2024-06-06 12:08:19 +0100

    Delete portafolioo directory

--- portafolioo/js/funciones.js
+++
@@ -1,6 +0,0 @@
-function estilos(ruta) {
-    const link = document.createElement("link");
-    link.setAttribute("rel","stylesheet");
-    link.setAttribute("href",ruta);
-    document.head.appendChild(link);
-}
--- portafolioo/js/portafolio.js
+++
@@ -1,2 +0,0 @@
-estilos("/portafolio/style/responsivo.css");
-estilos("/portafolio/style/portafolio.css");
